22812d48cbda5d866dabc8418bd3c278651cd563
Lakshmanra <99342842+Lakshmanra@users.noreply.github.com>
2022-02-11 09:34:48 +0530

    Create Pass or fail.py

---
+++ Pass or fail.py
@@ -0,0 +1,37 @@
+sno=int(input('enter number'))
+sname=input ('enter name')
+sgroup=input ('enter group')
+s1=int(input ('maths'))
+s2=int(input ('chemistry'))
+s3=int(input ('computer'))
+s4=int(input ('english'))
+s5=int(input ('telugu'))
+s6=int(input('online business'))
+s7=int(input('environment'))
+if s1>=35 and s2>=35 and s3>=35 and s4>=35 and s5>=35 and s6>=35 and s7>=35:
+    print ("pass")
+else:
+    print ("fail")
+
+Output:
+enter number
+1
+enter name
+Lakshman
+enter group
+Mccs
+maths
+98
+chemistry
+99
+computer
+96
+english
+99
+telugu
+100
+online business
+99
+environment
+88
+pass
